b227acb3b909852b5cb29ecc7b21850ee6929ca8
Valentin, Hetriuc <valentinconstantin.hetriuc@cgm.com>
2017-09-13 18:04:21 +0300

    m

--- src/index.js
+++ src/index.js
@@ -5,3 +5,40 @@ import numeral from 'numeral'; //lib for formating numbers
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${courseValue} for this awesome course!`);
+
+console.log("hello world!");
+console.warn("this is a warning!");
+console.error("this is a error");
+console.info("This is info!");
+console.debug("This is a debug!");
+
+console.count();
+console.group();
+console.groupCollapsed();
+console.groupEnd()
+
+count.assert();
+cosnole.table();
+
+ var j = 0;
+for ( var i = 1; i <= 100; i++) {
+ 
+	if ( (i % 3) == 0 && !(i % 5) == 0 ){
+    	console.log("Fizz");
+      continue;
+    } 
+    
+    if ((i % 5) == 0 && !(i % 3) == 0) {
+    	console.log("Buzz");
+      continue;
+    } 
+  
+  if( (i % 3) == 0 && (i % 5) == 0 ) {
+  	console.log("FizzBuzz");
+    j++;
+    console.log("total apparition for FizzBuzz " + j);
+    continue;
+  }
+  
+  	console.log(i);
+}
